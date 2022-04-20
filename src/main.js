54b73a799f63075c29c102830ff5253d4e7aa405
Sean Lee <seankyubinlee@gmail.com>
2022-04-19 19:43:46 -0700

    edit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
 //12 hours to complete
 //Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
 //Implement mouse control for player movement and mouse click to fire (20)
+//Display the time remaining (in seconds) on the screen (10)
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+//total points 110
 let config = {
   type: Phaser.AUTO,
   width: 640,
